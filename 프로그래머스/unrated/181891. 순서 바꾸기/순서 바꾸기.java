73e7f7284c58d1d451b1d2aa984bcaea1b7aa35b
minju02 <55305085+minju02@users.noreply.github.com>
2023-08-02 19:20:22 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 66.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public List solution(int[] num_list, int n) {
+        List answer = new ArrayList<>();
+        
+        for(int i = n; i < num_list.length; i++){
+            answer.add(num_list[i]);
+        }
+        
+        for(int i = 0; i < n; i++){
+            answer.add(num_list[i]);
+        }
+        
+        return answer;
+    }
+}
