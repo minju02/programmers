59582bafead200214be86f34ae63d15d7dfec050
minju02 <55305085+minju02@users.noreply.github.com>
2023-08-13 14:56:27 +0900

    [unrated] Title: 정수 부분, Time: 0.31 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181850. 정수 부분/정수 부분.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(double flo) {
+        int answer = 0;
+        
+        answer = (int)Math.floor(flo);
+        
+        return answer;
+    }
+}
